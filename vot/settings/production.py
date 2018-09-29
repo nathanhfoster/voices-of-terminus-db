4a4299a1b35651d994e8d4fad5b74a6ad99b070c
Nathan <nateinthegame@gmail.com>
2018-09-28 21:28:11 -0700

    Test

--- vot/settings/production.py
+++ vot/settings/production.py
@@ -104,8 +104,7 @@ DATABASES = {
 
 import dj_database_url
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
