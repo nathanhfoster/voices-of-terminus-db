f10ff385ad0cc827208bfb72fe45daea31748762
Nathan <nateinthegame@gmail.com>
2018-10-13 15:34:14 -0700

    AllowAny on Articles and Newsletters GET

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,18 @@
 from .models import Document
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import AllowAny
 from .serializers import DocumentSerializer
+from articles.permissions import IsOwnerOrReadOnly
 
 class DocumentView(viewsets.ModelViewSet):
     serializer_class = DocumentSerializer
     queryset = Document.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_permissions(self):
+        # allow an authenticated user to create via POST
+        if self.request.method == 'GET':
+            self.permission_classes = (AllowAny,)
+        if self.request.method == 'PATCH':
+            self.permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly,)
+        return super(DocumentView, self).get_permissions()
--- newsletters/views.py
+++ newsletters/views.py
@@ -1,7 +1,18 @@
 from .models import NewsLetter
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import AllowAny
 from .serializers import NewsLetterSerializer
+from newsletters.permissions import IsOwnerOrReadOnly
 
 class NewsLetterView(viewsets.ModelViewSet):
     serializer_class = NewsLetterSerializer
     queryset = NewsLetter.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_permissions(self):
+        # allow an authenticated user to create via POST
+        if self.request.method == 'GET':
+            self.permission_classes = (AllowAny,)
+        if self.request.method == 'PATCH':
+            self.permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly,)
+        return super(NewsLetterView, self).get_permissions()
