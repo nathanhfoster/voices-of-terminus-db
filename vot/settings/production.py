38708b76910500b8dcfae7605786d3b0aa08ecc0
Nathan <nateinthegame@gmail.com>
2018-10-24 15:52:14 -0700

    django_heroku

--- vot/settings/production.py
+++ vot/settings/production.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -205,3 +206,4 @@ CORS_ALLOW_HEADERS = (
         'x-csrftoken',
         'cache-control',
 )
+django_heroku.settings(locals())
