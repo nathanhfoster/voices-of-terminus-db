14c2bcbb62a1105ef5e531c49e2398abf8a6ed60
Nathan <nateinthegame@gmail.com>
2018-11-10 09:18:09 -0800

    Updated permissions

--- vot/settings/production.py
+++ vot/settings/production.py
@@ -187,7 +187,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-# CORS_ORIGIN_WHITELIST = ('*')
+CORS_ORIGIN_WHITELIST = ('*')
 CORS_ALLOW_METHODS = (
     'GET',
     'POST',
