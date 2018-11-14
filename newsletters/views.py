1a9b66a0455dbe7a280b0d41a64d761828fbd328
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-11-14 10:09:49 -0800

    Add counter to newsletters

--- newsletters/views.py
+++ newsletters/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets, permissions
 from rest_framework.permissions import AllowAny
 from .serializers import NewsLetterSerializer
 from newsletters.permissions import IsOwnerOrReadOnly, IsUpdateProfile
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class NewsLetterView(viewsets.ModelViewSet):
     serializer_class = NewsLetterSerializer
@@ -16,3 +18,13 @@ class NewsLetterView(viewsets.ModelViewSet):
         if self.request.method == 'PATCH':
             self.permission_classes = (permissions.IsAuthenticated, IsUpdateProfile)
         return super(NewsLetterView, self).get_permissions()
+
+    @action(methods=['get'], detail=True, permission_classes=[permission_classes])
+    def view(self, request, pk):
+        # TODO Check that the object exist
+        # Query database for the object with the given PK
+        qs = NewsLetter.objects.get(pk=pk)
+        # Increment the view count
+        qs.views += 1
+        qs.save()  # save
+        return Response(NewsLetterSerializer(qs).data)
