3b90f3b4f254b0634c8c05d4b2ece70cd38e7b7d
Nathan <nateinthegame@gmail.com>
2018-11-15 10:38:27 -0800

    Get Users Without Profile Pictures

--- user/views.py
+++ user/views.py
@@ -8,6 +8,7 @@ from .serializers import UserSerializer
 from user.permissions import IsUpdateProfile, IsStaffOrTargetUser
 from rest_framework.decorators import action
 from rest_framework.response import Response
+import logging
 
 
 class UserView(viewsets.ModelViewSet):
@@ -41,6 +42,12 @@ class UserView(viewsets.ModelViewSet):
 
         return Response(Serializer(qs).data)
 
+    @action(methods=['get'], detail=False, permission_classes=[permission_classes])
+    def all(self, request):
+        qs = User.objects.all()
+        serializer = AdminSerializer(qs, many=True)
+        return Response(serializer.data)
+
 class Serializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -53,4 +60,23 @@ class Serializer(serializers.ModelSerializer):
         'can_update_article', 'can_update_newsletter', 'can_update_calendar_event',
         'can_delete_article', 'can_delete_newsletter', 'can_delete_calendar_event',
         'is_active', 'experience_points', 'guild_points'
+        )
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+        'id', 'username', 'first_name', 'last_name',
+        'bio', 'primary_race', 'primary_role', 'secondary_class', 'secondary_race', 'secondary_role',
+        'profession', 'profession_specialization',
+        'is_superuser', 'email', 'is_staff', 'is_leader', 'is_council','is_general_officer', 'is_officer',
+        'is_senior_member', 'is_junior_member', 'is_recruit',
+        'is_raid_leader', 'is_banker', 'is_recruiter', 'is_class_lead', 'is_crafter_lead',
+        'can_create_article', 'can_create_newsletter', 'can_create_calendar_event',
+        'can_read_article', 'can_read_newsletter', 'can_read_calendar_event',
+        'can_update_article', 'can_update_newsletter', 'can_update_calendar_event',
+        'can_delete_article', 'can_delete_newsletter', 'can_delete_calendar_event',
+        'is_active', 'date_joined', 'last_login', 'experience_points', 'guild_points',
+        'discord_url', 'twitter_url', 'twitch_url', 'youtube_url',
+        'primary_class'
         )
