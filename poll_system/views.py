f726f2b1636ee7bef09bfec873c0323aab024f5d
Nathan <nateinthegame@gmail.com>
2019-01-22 12:40:02 -0800

    PAGE SIZE

--- poll_system/views.py
+++ poll_system/views.py
@@ -11,13 +11,13 @@ from .serializers import PollSeializer, PollQuestionSeializer, PollResponseSeial
 
 
 class StandardResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 20
+    page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 100
 
 
 class LargeResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 100
+    page_size = 500
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
